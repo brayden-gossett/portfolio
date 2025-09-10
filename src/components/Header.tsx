7f02b270a877a34a33ba015f94677705f86c5a4b
brayden-gossett <brayden.gossett10@gmail.com>
2025-09-09 21:02:28 -0700

    Fixed Image Issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,21 @@
 import "../App.css";
+import headshot from "../assets/headshot.png";
 
 function Header() {
     return (
-        <>
-            <div className="hero-section">
-                <div className="words">
-                    <h1>Hello my name is Brayden</h1>
-                    <h3 className="thing2"> An almost Software Developer </h3>
-                </div>
-                <img
-                    className="headshot"
-                    src="/portfolio/src/assets/headshot.png"
-                    width={340}
-                    height={420}
-                />
+        <div className="hero-section">
+            <div className="words">
+                <h1>Hello my name is Brayden</h1>
+                <h3 className="thing2">An almost Software Developer</h3>
             </div>
-        </>
+            <img
+                className="headshot"
+                src={headshot}
+                width={340}
+                height={420}
+                alt="Headshot"
+            />
+        </div>
     );
 }
 
